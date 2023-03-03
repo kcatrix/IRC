29d205801c4edc7384e5b055c1272456f8896545
Benjamin Leotard <bleotard@student.42nice.fr>
2023-03-03 22:21:45 +0100

    join seems functional

--- srcs/join.cpp
+++ srcs/join.cpp
@@ -17,11 +17,23 @@ Channel     findChannel (std::string channel_name, Server& irc_server) {
     return Channel ();
 }
 
-void    addUser (User executer, std::string channel_name, Server& irc_server) {
+int     findUser (Channel to_join, std::string nickname) {
+    for (std::vector<std::string>::iterator it = to_join.chan_users.begin (); it != to_join.chan_users.end (); it++) {
+        if ((*it) == nickname)
+            return 1;
+    }
+    return 0;
+}
+
+void    addUser (User executer, Channel to_join, Server& irc_server) {
     for (std::vector<Channel>::iterator it = irc_server.channels.begin (); it != irc_server.channels.end (); it++) {
-        if ((*it).channel_name == channel_name) {
-            (*it).chan_users.push_back (executer.nickname);
-            print_message (executer.sd, "You joined the channel #" + (*it).channel_name + "\n");
+        if ((*it).channel_name == to_join.channel_name) {
+            if (findUser (to_join, executer.nickname) == 0) {
+                (*it).chan_users.push_back (executer.nickname);
+                print_message (executer.sd, "You joined the channel #" + (*it).channel_name + "\n");
+            }
+            else
+                print_message (executer.sd, "You are already in the channel #" + (*it).channel_name + "\n");
         }
     }
 }
@@ -36,14 +48,6 @@ void    join (User executer, char* buffer, Server& irc_server) {
     Channel to_join = findChannel (channel_name, irc_server);
     if (to_join.channel_name.empty () == 1)
         createChannel (executer, channel_name, irc_server);
-    else {
-        addUser (executer, channel_name, irc_server);
-        //(*it).chan_users.push_back (executer.nickname);
-        print_message (executer.sd, "You joined the channel #" + channel_name + "\n");
-        return;
-    }
-    std::cout << "List of channels: " << std::endl;
-    for (std::vector<Channel>::iterator it = irc_server.channels.begin (); it != irc_server.channels.end (); it++)
-        std::cout << (*it).channel_name << std::endl;
-    std::cout << "end of list" << std::endl;
+    else
+        addUser (executer, to_join, irc_server);
 }
