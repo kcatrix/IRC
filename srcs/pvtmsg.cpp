8efb9163dd8425e80c26f38b218753b605d9ec57
Thomas Nicoue <tnicoue@c3r2p3.42nice.fr>
2023-03-08 10:55:45 +0100

    Fix msg

--- srcs/pvtmsg.cpp
+++ srcs/pvtmsg.cpp
@@ -2,12 +2,22 @@
 
 //  ---------------(fonctionne dans l'ensemble hors mask(pas implemente car manque d'info sans les channs) + mauvais sender car prob de socket de discord)--------------------------------------
 
+static int isInChan(User executer, CHANNEL_ITERATOR chan)
+{
+	for (USER_ITERATOR it = chan->chan_users.begin (); it != chan->chan_users.end (); it++)
+	{
+		if(executer.nickname == (*it).nickname)
+			return (1);
+	}
+	return 0;
+}
+
 void msg(User executer, std::vector<std::string> bufferSplit, std::vector<User> users, Server& irc_server) {
 
     //protect segfault
     (void)irc_server;
 	std::string message;
-    if(bufferSplit[1].empty() == 1 || bufferSplit[2].empty() == 1 || bufferSplit[3].empty() == 1)
+    if(bufferSplit[1].empty() == 1 || bufferSplit[2].empty() == 1)
         return ;
     if (bufferSplit[1][0] != '#')
     {
@@ -33,20 +43,25 @@ void msg(User executer, std::vector<std::string> bufferSplit, std::vector<User> 
     else
     {
         CHANNEL_ITERATOR to_send = findChannel (bufferSplit[1], irc_server);
-        for (USER_ITERATOR it = to_send->chan_users.begin (); it != to_send->chan_users.end (); it++)
+        if(isInChan(executer, to_send) == 0)
+            print_message (executer.sd, "You cannot send message in a channel you're not into\n");
+        else
         {
-            std::cout << "away == " << (*it).away << std::endl; 
-            if ((*it).away == true )// ne fonctionne pas car pas de away == false meme aprés fonction away           
+            for (USER_ITERATOR it = to_send->chan_users.begin (); it != to_send->chan_users.end (); it++)
             {
-                message = (*it).away_message + '\n';
-                print_message ((*it).sd, message);
-                return ;
-            } 
-            if(bufferSplit[2].empty () == 0)
-                message = bufferSplit[2];
-            for(int y = 3; bufferSplit[y].empty () == 0; y++)
-                message = message + " " + bufferSplit[y];
-            print_message ((*it).sd, "<" + executer.nickname + "> " + message + "\n");
+                std::cout << "away == " << (*it).away << std::endl; 
+                if ((*it).away == true )// ne fonctionne pas car pas de away == false meme aprés fonction away           
+                {
+                    message = (*it).away_message + '\n';
+                    print_message ((*it).sd, message);
+                    return ;
+                } 
+                if(bufferSplit[2].empty () == 0)
+                    message = bufferSplit[2];
+                for(int y = 3; bufferSplit[y].empty () == 0; y++)
+                    message = message + " " + bufferSplit[y];
+                print_message ((*it).sd, "<" + executer.nickname + "> " + message + "\n");
+            }
         }
     }
 }
