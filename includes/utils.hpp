dd4306e68f52d03d3a8323d5e4da1baa89580634
Benjamin Leotard <bleotard@student.42nice.fr>
2023-03-09 00:51:27 +0100

    got rid of char* and removed unused util functions

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -4,16 +4,4 @@
 
 void    print_error (std::string error_message);
 void    print_message (int sd, std::string message);
-bool    CheckUserExists (std::vector<User> users, int fd);
-void    removeInvisibleChars(char* str);
-int     removeStringVector(std::vector<std::string>& myVector, const char* target);
-STRING_VECTOR     splitString(const std::string& string);
-
-
-
-void	free_tab(char **str);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	**ft_split(char const *s, char c);
-size_t	ft_strlen(const char *s);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s1);
+STRING_VECTOR     splitString( std::string& string);
